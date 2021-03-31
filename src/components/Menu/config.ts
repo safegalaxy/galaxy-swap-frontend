f31a29c5767685dbf2f89bbd6e662aa4fec8db04
Fenton Haslam <fenton@Fentons-MacBook-Pro.local>
2021-03-31 07:54:44 +0100

    chore: removed info and more from nav bar

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -49,50 +49,6 @@ const config: MenuEntry[] = [
       },
     ],
   },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'Overview',
-        href: 'https://pancakeswap.info',
-      },
-      {
-        label: 'Tokens',
-        href: 'https://pancakeswap.info/tokens',
-      },
-      {
-        label: 'Pairs',
-        href: 'https://pancakeswap.info/pairs',
-      },
-      {
-        label: 'Accounts',
-        href: 'https://pancakeswap.info/accounts',
-      },
-    ],
-  },
-  {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: 'Voting',
-        href: 'https://voting.pancakeswap.finance',
-      },
-      {
-        label: 'Github',
-        href: 'https://github.com/pancakeswap',
-      },
-      {
-        label: 'Docs',
-        href: 'https://docs.pancakeswap.finance',
-      },
-      {
-        label: 'Blog',
-        href: 'https://pancakeswap.medium.com',
-      },
-    ],
-  },
 ]
 
 export default config
